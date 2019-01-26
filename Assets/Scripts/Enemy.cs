6a9a15dc8315eb0dfccd2ecb64120fafcb514a4a
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-01-26 02:37:17 +0200

    Add enemy attack. Improved enemy behaviour

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,12 +9,20 @@ public class Enemy : Unit {
 	private static Treehouse house;
 
 	public int damage;
+	public float attackRange;
+	public float attackPushback;
 	public float attackCooldown;
+
+	private float lastAttack;
 	private NavMeshAgent agent;
 	private Unit target;
 
 	public void SetTarget(Unit target) {
 		this.target = target;
+	}
+
+	protected override void Die() {
+		Destroy(this.gameObject);
 	}
 
 	private void Start() {
@@ -30,14 +38,31 @@ public class Enemy : Unit {
 	}
 
 	private void Update() {
+		if(!agent.enabled) { return; }
+		
 		if(!target.enabled) {
 			target = house;
 		}
 
 		agent.SetDestination(target.transform.position);
+
+		if(Vector3.Distance(target.transform.position, transform.position) <= attackRange) {
+			Attack();
+		}		
 	}
 
-	protected override void Die() {
-		Destroy(this.gameObject);
+	private void Attack() {
+		if((Time.time - lastAttack) > attackCooldown) {
+			target.TakeDamage(damage);
+			Rigidbody enRigidbody = target.GetComponent<Rigidbody>();
+			if(enRigidbody != null) {
+				enRigidbody.AddForce((target.transform.position - transform.position).normalized * attackPushback, ForceMode.Impulse);
+			}
+			lastAttack = Time.time;
+			agent.enabled = false;
+			CustomCoroutine.WaitThenExecute(attackCooldown, () => agent.enabled = true);
+		}
 	}
+
+	
 }
