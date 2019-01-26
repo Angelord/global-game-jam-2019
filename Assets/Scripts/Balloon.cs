8403d918aa0d9bae38088fe9830c12774f50c2cd
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-01-26 12:24:54 +0200

    Add varying speed for balloon

--- Assets/Scripts/Balloon.cs
+++ Assets/Scripts/Balloon.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Balloon : MonoBehaviour {
 
-	public int damage;
-	public float speed = 10.0f;
-	public float travelDist = 5.0f;
-	public GameObject explosion;
+	[SerializeField] private int damage;
+	[SerializeField] private float riseSpeed = 6.0f;
+	[SerializeField] private float fallSpeed = 12.0f;
+	[SerializeField] private float travelDist = 5.0f;
+	[SerializeField] private GameObject explosion;
 	[SerializeField] private float maxSize;
 	[SerializeField] private float minSize;
 
+	private float speed = 10.0f;
 	private Vector3 startPos;
 	private Vector3 movDir;
 	private Direction direction;
@@ -21,17 +23,23 @@ public class Balloon : MonoBehaviour {
 	}
 
 	public void Launch(Direction direction) {
+		this.speed = riseSpeed;
 		this.direction = direction;
 		this.startPos = transform.position;
 
 		movDir = DirUtil.DirectionToVec(direction);
-		
 	}
 
 	private void Update() {
+
 		transform.Translate(movDir * speed * Time.deltaTime);
 
 		float distanceTraveled = (transform.position - startPos).magnitude;
+
+		if(distanceTraveled > travelDist / 2) {
+			speed = fallSpeed;
+		}
+
 		if(distanceTraveled >= travelDist) {
 			Explode();
 		}
