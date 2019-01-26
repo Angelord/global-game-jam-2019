c0d2d873535b4c4599464dabf23605c4fc93e385
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-01-26 03:01:33 +0200

    Add enemy spawner

---
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+	
+    public float breakTimes = 1.0f;
+    public List<Wave> waves = new List<Wave>();
+
+    private int waveIndex = 0;
+    private int enemiesDead;
+
+    private Wave CurrentWave { get { return waves[waveIndex]; } }
+
+    private void Start() {
+        StartCoroutine(SpawnNextWave());
+    }
+
+    private IEnumerator SpawnNextWave() {
+        yield return new WaitForSeconds(breakTimes);
+
+        for(int i = 0; i < CurrentWave.numEnemies; i++) {
+            Instantiate(CurrentWave.enemyType, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(CurrentWave.spawnFrequency);
+        }
+    }   
+}
---
+++ Assets/Scripts/Enemies/Wave.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Wave {
+
+	public float spawnFrequency = 0.8f;
+	public GameObject enemyType;
+	public int numEnemies;
+}
