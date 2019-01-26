d04158a3f0b69edbee53d90667263048e74b9a96
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-01-26 02:03:35 +0200

    Enemies now move towards treehouse

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,13 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour {
+
+	private static Treehouse house;
 
 	public int health;
 	public int damage;
 	public float attackCooldown;
+	private NavMeshAgent agent;
 
+	private void TryFindTreehouse() {
+		if(house == null) {
+			house = GameObject.FindGameObjectWithTag("Treehouse").GetComponent<Treehouse>();
+		}
+	}
+
+	private void Start() {
+		TryFindTreehouse();
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(house.transform.position);
+	}
 
 	public void TakeDamage(int amount) {
 		health -= amount;
