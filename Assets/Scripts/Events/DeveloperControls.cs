0bc71d466d72a5cf2bc4f41c0318de52ac3c22b5
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-01-27 15:17:11 +0200

    Change player input mapping

---
+++ Assets/Scripts/Events/DeveloperControls.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeveloperControls : MonoBehaviour {
+
+	void Update () {
+		// if(Input.GetKey(KeyCode.LeftControl) && Input.GetKey(KeyCode.LeftShift) && Input.GetKey()) {
+
+		// }		
+	}
+}
