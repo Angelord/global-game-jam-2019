ef060640d54bd1a7f24128c24fd310e47958ed93
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-01-27 04:34:55 +0200

    Create banana

---
+++ Assets/Scripts/Objects/Banana.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Insta-kills an enemy that steps on it
+public class Banana : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "Enemy") {
+			Unit unitComp = other.gameObject.GetComponent<Unit>();
+			if(unitComp != null) {
+				unitComp.TakeDamage(unitComp.CurHealth);
+				Destroy(this.gameObject);
+			}
+		}
+	}
+}
