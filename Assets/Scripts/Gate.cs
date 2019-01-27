23d2692089d9884a82dfb888476171ff5c35d30a
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-01-27 03:12:53 +0200

    Add new glowing fence

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Gate : MonoBehaviour {
 
+	[SerializeField] private Animator fence; 
+
 	public void Open() {
 		
 		this.GetComponentInChildren<Collider>().enabled = false;
 		this.GetComponentInChildren<Animator>().SetTrigger("Open");
+
+		fence.SetTrigger("Light");
 	}
 }
