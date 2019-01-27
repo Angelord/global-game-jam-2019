ae4eadee1af7d526030a1a2968340a2540cfd829
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-01-27 10:37:10 +0200

    Improve candy dispersing

--- Assets/Scripts/Enemies/LootDropper.cs
+++ Assets/Scripts/Enemies/LootDropper.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class LootDropper : MonoBehaviour {
 
 	[SerializeField] private GameObject lootPrefab;
+	[SerializeField] private int minAmount = 1;
+	[SerializeField] private int maxAmount = 1;
 
 	public void Drop() {
 		if(lootPrefab != null) {
-			Instantiate(lootPrefab, transform.position, Quaternion.identity);
+			int amountToDrop = Random.Range(minAmount, maxAmount + 1);
+			for(int i = 0; i < amountToDrop; i++) {
+
+				float xOffset = Random.Range(-1.0f, 1.0f);
+				float zOffset = Random.Range(-1.0f, 1.0f);
+				Vector3 spawnPos = transform.position + new Vector3(xOffset, 0.0f, zOffset);
+
+				GameObject loot = Instantiate(lootPrefab, spawnPos, Quaternion.identity);
+
+				Vector3 forceDir = spawnPos - transform.position;
+				forceDir.Normalize();
+				
+				loot.GetComponent<Rigidbody>().AddForce(forceDir * 1.9f, ForceMode.Impulse);
+			}
 			Destroy(this);
 		}
 	}
