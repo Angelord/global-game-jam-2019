2d7d0e58b1258ca2b76108fa341094bd5cbe8620
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-01-28 20:40:29 +0200

    Add instant house upgrade preview to shop

---
+++ Assets/Scripts/ShopTreehouse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopTreehouse : MonoBehaviour {
+
+	private Animator animator;
+
+	private void Start() {
+		animator = GetComponentInChildren<Animator>();
+	}
+
+	private void Update() {
+		if(animator.GetInteger("Level") != Progress.HouseLevel) {
+			animator.SetInteger("Level", Progress.HouseLevel);
+		}
+	}
+}
